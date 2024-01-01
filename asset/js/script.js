fff36af58c7668458a17f715847fd5338ec254cd
Sina Salimirad <138554159+sinasalimirad@users.noreply.github.com>
2024-01-01 16:47:26 +0330

    navbar & sidebar (responsive)
    navbar & sidebar HTML , CSS and JS code is finished (Responsive )

--- asset/js/script.js
+++ asset/js/script.js
@@ -4,3 +4,17 @@ let preloader = document.querySelector('[preloader]');
 window.addEventListener('load', () => {
     preloader.classList.add('loaded');
 });
+
+// ----- Menu Toggle -----
+let menuToggle = document.querySelector('[menuToggle]'),
+    sidebar = document.querySelector('[sidebar]');
+
+menuToggle.addEventListener('click', () => {
+    if (menuToggle.classList.contains('bx-menu-alt-right')) {
+        menuToggle.classList.replace('bx-menu-alt-right', 'bx-x');
+        sidebar.classList.add('open');
+    } else {
+        menuToggle.classList.replace('bx-x', 'bx-menu-alt-right');
+        sidebar.classList.remove('open');
+    }
+});
